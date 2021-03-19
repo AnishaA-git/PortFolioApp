3ccf507d90bf0a2d96c677c57095d40ea98d04e8
AnishaA-git <anisha.agarwal1993@gmail.com>
2021-03-19 16:56:39 -0700

    Code for Navigation between fragments add

--- app/src/main/java/com/example/portfolio/MainActivity.kt
+++ app/src/main/java/com/example/portfolio/MainActivity.kt
@@ -2,16 +2,24 @@ package com.example.portfolio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
+import com.example.portfolio.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation)
         val navController = Navigation.findNavController(this, R.id.fragment)
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
+
 }
--- app/src/main/java/com/example/portfolio/fragments/FragmentHome.kt
+++ app/src/main/java/com/example/portfolio/fragments/FragmentHome.kt
@@ -1,11 +1,16 @@
 package com.example.portfolio.fragments
 
 import android.os.Bundle
+import android.view.*
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.portfolio.R
+import com.example.portfolio.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -17,7 +22,25 @@ class FragmentHome : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        //return inflater.inflate(R.layout.fragment_home, container, false)
+        //val textView = findViewById<TextView>(R.id.txt_projects)
+
+        val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,
+            R.layout.fragment_home,container,false)
+
+        binding.Project.setOnClickListener { view: View ->
+            //view.findNavController().navigate(R.id.action_titleFragment_to_questionFragment)
+            Navigation.findNavController(view).navigate(R.id.action_fragmentHome_to_fragmentProjects)
+
+        }
+        binding.Contact.setOnClickListener { view: View ->
+            //view.findNavController().navigate(R.id.action_titleFragment_to_questionFragment)
+            Navigation.findNavController(view).navigate(R.id.action_fragmentHome_to_fragmentContact)
+
+        }
+
+        //setHasOptionsMenu(true)
+        return binding.root
     }
 
 }

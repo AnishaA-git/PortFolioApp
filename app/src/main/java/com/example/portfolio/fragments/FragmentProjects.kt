feba18b8448311b725e36e627982db4fe913940d
AnishaA-git <anisha.agarwal1993@gmail.com>
2021-03-19 19:11:28 -0700

    findviewbyid replaced for fragment project

--- app/src/main/java/com/example/portfolio/fragments/FragmentProjects.kt
+++ app/src/main/java/com/example/portfolio/fragments/FragmentProjects.kt
@@ -5,10 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.GridView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.portfolio.AdapterClient
 import com.example.portfolio.ClientModel
 import com.example.portfolio.R
+import com.example.portfolio.databinding.ActivityMainBinding
+import com.example.portfolio.databinding.FragmentHomeBinding
+import com.example.portfolio.databinding.FragmentProjectsBinding
 import java.util.*
 
 /**
@@ -18,18 +22,25 @@ import java.util.*
 class FragmentProjects : Fragment() {
     var clientModel: MutableList<ClientModel>? = null
     var gridView: GridView? = null
+    private lateinit var binding: FragmentProjectsBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_projects, container, false)
+        //return inflater.inflate(R.layout.fragment_projects, container, false)
+
+         binding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_projects,container,false)
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        gridView = view.findViewById(R.id.gridView)
+
         clientModel = ArrayList()
         val clientModel = clientModel;
-        val gridView = gridView;
+        val gridView = binding.gridView;
         if (clientModel != null) {
             clientModel.add(ClientModel("Amazon", R.drawable.amazon))
             clientModel.add(ClientModel("Apple", R.drawable.apple))

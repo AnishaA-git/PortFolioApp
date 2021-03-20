e21db18b214f7004b64b1fc609de7e200f3b4a35
AnishaA-git <anisha.agarwal1993@gmail.com>
2021-03-19 18:22:15 -0700

    Up Button added

--- app/src/main/java/com/example/portfolio/MainActivity.kt
+++ app/src/main/java/com/example/portfolio/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.portfolio
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.example.portfolio.databinding.ActivityMainBinding
@@ -10,6 +11,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +20,17 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation)
-        val navController = Navigation.findNavController(this, R.id.fragment)
+        //val navController = Navigation.findNavController(this, R.id.fragment)
+        navController = Navigation.findNavController(this, R.id.fragment)
+
+        NavigationUI.setupActionBarWithNavController(this,navController)
+
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = Navigation.findNavController(this, R.id.fragment)
+        return navController.navigateUp()
+    }
 }
